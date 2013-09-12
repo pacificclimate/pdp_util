0a567ee80f2ad561c5cb77aea521b92b5023ae44
Basil Veerman <bveerman@uvic.ca>
2013-09-12 11:56:00 -0700

    minor version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ class PyTest(TestCommand):
         errno = pytest.main(self.test_args)
         sys.exit(errno)                                                                        
 
-__version__ = (0, 1, 1)
+__version__ = (0, 1, 2)
 
 sw_path = 'hg+ssh://medusa.pcic.uvic.ca//home/data/projects/comp_support/software'
 
