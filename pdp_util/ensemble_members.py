99a97f8ddc3765d0dc5815b36ffdf76ad922d9fb
Basil Veerman <bveerman@uvic.ca>
2013-11-15 14:44:44 -0800

    Create custom lister classes for specific portals

--- pdp_util/ensemble_members.py
+++ pdp_util/ensemble_members.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from webob.request import Request
 from simplejson import dumps
 from numpy import array
@@ -25,15 +27,16 @@ class EnsembleMemberLister(object):
 
             if not ensemble: # Result does not contain any row therefore ensemble does not exist
                 start_response('404 Not Found', [])
-                return ["Requested ensemble does not exist"]
+                return ["Requested ensemble does not exist or does not contain any members"]
 
             tuples = [x for x in self.list_stuff(ensemble)] # query is lazy load, so must be assigned within scope
 
         status = '200 OK'
         response_headers = [('Content-type','application/json; charset=utf-8')]
         start_response(status, response_headers)
-        return dumps(self.dictify(array(tuples)))
-            
+        d = OrderedDict(sorted(dictify(array(tuples)).items(), key=lambda t:t[0]))
+        return dumps(d)
+
     def list_stuff(self, ensemble):
         for ensemble_run in ensemble.ensemble_runs:
             run = ensemble_run.run
@@ -41,9 +44,31 @@ class EnsembleMemberLister(object):
                 for data_file_variable in file_.data_file_variables:
                     yield run.emission.short_name, run.model.short_name, run.name, data_file_variable.netcdf_variable_name, file_.unique_id.replace('+', '-') # FIXME: kill the replacement once ncWMS stops being dumb
 
-    def dictify(self, a):
-        if len(a) == 1:
-            return a.flatten()[0]
-        else:
-            keys = set(a[:,0])
-            return { key: self.dictify(a[ array([ val == key for val in a[:,0] ], dtype=bool) ,1:]) for key in keys }
+class PrismEnsembleLister(EnsembleMemberLister):
+    def __init__(self, dsn):
+        super(PrismEnsembleLister, self).__init__(dsn)
+    
+    def list_stuff(self, ensemble):
+        for ensemble_run in ensemble.ensemble_runs:
+            run = ensemble_run.run
+            for file_ in run.files:
+                for data_file_variable in file_.data_file_variables:
+                    yield run.model.short_name, data_file_variable.netcdf_variable_name, file_.unique_id.replace('+', '-') # FIXME: kill the replacement once ncWMS stops being dumb
+
+class DownscaledEnsembleLister(EnsembleMemberLister):
+    def __init__(self, dsn):
+        super(DownscaledEnsembleLister, self).__init__(dsn)
+    
+    def list_stuff(self, ensemble):
+        for ensemble_run in ensemble.ensemble_runs:
+            run = ensemble_run.run
+            for file_ in run.files:
+                for data_file_variable in file_.data_file_variables:
+                    yield run.emission.short_name, run.model.short_name, data_file_variable.netcdf_variable_name, file_.unique_id.replace('+', '-') # FIXME: kill the replacement once ncWMS stops being dumb
+
+def dictify(a):
+    if len(a) == 1:
+        return a.flatten()[0]
+    else:
+        keys = set(a[:,0])
+        return { key: dictify(a[ array([ val == key for val in a[:,0] ], dtype=bool) ,1:]) for key in keys }
