905e7c89b6903b4a1353668cfa2980f5971e9667
Basil Veerman <bveerman@uvic.ca>
2013-11-15 12:58:09 -0800

    Convert to scoped session

--- pdp_util/ensemble_members.py
+++ pdp_util/ensemble_members.py
@@ -2,13 +2,15 @@ from webob.request import Request
 from simplejson import dumps
 from numpy import array
 
-import pdp_util
+from pdp_util import session_scope
 from modelmeta import *
 
 class EnsembleMemberLister(object):
     # TODO: Measure performance with thousands of elements
     def __init__(self, dsn):
-        self._session_factory = pdp_util.get_session(dsn)
+        def session_scope_factory():
+            return session_scope(dsn)
+        self.session_scope_factory = session_scope_factory
 
     def __call__(self, environ, start_response):
         req = Request(environ)
@@ -18,30 +20,30 @@ class EnsembleMemberLister(object):
             start_response('400 Bad Request', [])
             return ["Required parameter 'ensemble_name' not specified"]
 
-        sesh = self._session_factory()
-        ensemble = sesh.query(Ensemble).filter(Ensemble.name == ensemble_name).first() # FIXME: Could raise error
+        with self.session_scope_factory() as sesh:
+            ensemble = sesh.query(Ensemble).filter(Ensemble.name == ensemble_name).first()
+
+            if not ensemble: # Result does not contain any row therefore ensemble does not exist
+                start_response('404 Not Found', [])
+                return ["Requested ensemble does not exist"]
+
+            tuples = [x for x in self.list_stuff(ensemble)] # query is lazy load, so must be assigned within scope
 
         status = '200 OK'
         response_headers = [('Content-type','application/json; charset=utf-8')]
         start_response(status, response_headers)
-        if not ensemble:
-            return []
-        
-        def list_stuff():
-            for ensemble_run in ensemble.ensemble_runs:
-                run = ensemble_run.run
-                for file_ in run.files:
-                    for data_file_variable in file_.data_file_variables:
-                        yield run.emission.short_name, run.model.short_name, run.name, data_file_variable.netcdf_variable_name, file_.unique_id.replace('+', '-') # FIXME: kill the replacement once ncWMS stops being dumb
+        return dumps(self.dictify(array(tuples)))
+            
+    def list_stuff(self, ensemble):
+        for ensemble_run in ensemble.ensemble_runs:
+            run = ensemble_run.run
+            for file_ in run.files:
+                for data_file_variable in file_.data_file_variables:
+                    yield run.emission.short_name, run.model.short_name, run.name, data_file_variable.netcdf_variable_name, file_.unique_id.replace('+', '-') # FIXME: kill the replacement once ncWMS stops being dumb
 
-        tuples = [x for x in list_stuff()]
-
-        def dictify(a):
-            if len(a) == 1:
-                return a.flatten()[0]
-            else:
-                keys = set(a[:,0])
-                return { key: dictify(a[ array([ val == key for val in a[:,0] ], dtype=bool) ,1:]) for key in keys }
-
-        sesh.close()
-        return dumps(dictify(array(tuples)))
+    def dictify(self, a):
+        if len(a) == 1:
+            return a.flatten()[0]
+        else:
+            keys = set(a[:,0])
+            return { key: self.dictify(a[ array([ val == key for val in a[:,0] ], dtype=bool) ,1:]) for key in keys }
