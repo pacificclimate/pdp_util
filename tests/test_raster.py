fcff30d5c408cb65f63d257537d0d3b6d8181dc8
Basil Veerman <bveerman@uvic.ca>
2014-06-04 11:22:37 -0700

    Fix db configurator test to allow different handler ordering

--- tests/test_raster.py
+++ tests/test_raster.py
@@ -24,17 +24,23 @@ def test_db_raster_catalog(mm_session, config):
 def test_db_raster_configurator(mm_session):
     args = [mm_session, 'A name', 'Version 0.0.0.1', '0.0', 'bc_prism', 'http://basalt.pcic.uvic.ca:8080/data/']
     result = db_raster_configurator(*args)
-    assert result == {'root_url': 'http://basalt.pcic.uvic.ca:8080/data/',
-                      'name': 'A name',
-                      'handlers': [{'url': 'tmax_monClim_PRISM_historical_run1_197101-200012.nc',
-                                    'file': '/home/data/climate/PRISM/dataportal/tmax_monClim_PRISM_historical_run1_197101-200012.nc'},
-                                   {'url': 'pr_monClim_PRISM_historical_run1_197101-200012.nc',
-                                    'file': '/home/data/climate/PRISM/dataportal/pr_monClim_PRISM_historical_run1_197101-200012.nc'},
-                                   {'url': 'tmin_monClim_PRISM_historical_run1_197101-200012.nc',
-                                    'file': '/home/data/climate/PRISM/dataportal/tmin_monClim_PRISM_historical_run1_197101-200012.nc'}],
-                      'version': 'Version 0.0.0.1',
-                      'ensemble': 'bc_prism',
-                      'api_version': '0.0'}
+    assert all([x in result.items() for x in {'root_url': 'http://basalt.pcic.uvic.ca:8080/data/',
+                                             'name': 'A name',
+                                             'version': 'Version 0.0.0.1',
+                                             'ensemble': 'bc_prism',
+                                             'api_version': '0.0'}.items()])
+
+def test_db_raster_configurator_handlers(mm_session):
+    # Handlers must be tested seperately because they are a list and order cannot be guaranteed on database query
+    args = [mm_session, 'A name', 'Version 0.0.0.1', '0.0', 'bc_prism', 'http://basalt.pcic.uvic.ca:8080/data/']
+    handlers = [{'url': 'tmax_monClim_PRISM_historical_run1_197101-200012.nc',
+                 'file': '/home/data/climate/PRISM/dataportal/tmax_monClim_PRISM_historical_run1_197101-200012.nc'},
+                {'url': 'pr_monClim_PRISM_historical_run1_197101-200012.nc',
+                 'file': '/home/data/climate/PRISM/dataportal/pr_monClim_PRISM_historical_run1_197101-200012.nc'},
+                {'url': 'tmin_monClim_PRISM_historical_run1_197101-200012.nc',
+                 'file': '/home/data/climate/PRISM/dataportal/tmin_monClim_PRISM_historical_run1_197101-200012.nc'}]
+    result = db_raster_configurator(*args)
+    assert all([x in result['handlers'] for x in handlers])
 
 @pytest.mark.parametrize('url', ['', '/url/is/irrellevant'])
 def test_ensemble_catalog(mm_dsn, config, url):
