497fa52166deff5fae54cb26415f2acce590f16d
Rod Glover <rglover@uvic.ca>
2019-09-27 14:12:24 -0700

    Extend API with list-oriented parameters

--- pdp_util/filters.py
+++ pdp_util/filters.py
@@ -42,14 +42,84 @@ def mk_mp_regex():
     return multipolygon
 
 form_filters = {
-    'from-date':             FormFilter('from-date',             r'[0-9]{4}/[0-9]{2}/[0-9]{2}', lambda x: cng.max_obs_time > datetime.strptime(x, '%Y/%m/%d')),
-    'to-date':               FormFilter('to-date',               r'[0-9]{4}/[0-9]{2}/[0-9]{2}', lambda x: cng.min_obs_time < datetime.strptime(x, '%Y/%m/%d')),
-    'network-name':          FormFilter('network-name',          r'[A-Za-z_]+',                 lambda x: cng.network_name == x),
-    'input-var':             FormFilter('input-var',             r'[a-z: _]+',                  lambda x: cng.vars.like('%{}%'.format(x))),
-    'input-freq':            FormFilter('input-freq',            r'(1-hourly|irregular|daily|12-hourly)', lambda x: cng.freq == x),
-    'input-polygon':         FormFilter('input-polygon',          mk_mp_regex(),                "ST_intersects(ST_GeomFromText('%s', 4326), the_geom)"),
-    'only-with-climatology': FormFilter('only-with-climatology', 'only-with-climatology',       lambda x: or_(cng.vars.like('%within%'), cng.vars.like('%over%')))
-    }
+    'from-date': FormFilter(
+        # Single valid date in format '%Y/%m/%d'
+        'from-date',
+        r'[0-9]{4}/[0-9]{2}/[0-9]{2}',
+        lambda x: cng.max_obs_time > datetime.strptime(x, '%Y/%m/%d')
+    ),
+
+    'to-date': FormFilter(
+        # Single valid date in format '%Y/%m/%d'
+        'to-date',
+        r'[0-9]{4}/[0-9]{2}/[0-9]{2}',
+        lambda x: cng.min_obs_time < datetime.strptime(x, '%Y/%m/%d')
+    ),
+
+    'network-name': FormFilter(
+        # Comma-separated list (as string) of network names
+        'network-name',
+        # The following simple regex for a comma-separated list does not exclude
+        # empty list items (two successive commas) ... but it is simple.
+        r'[A-Za-z_,]*',
+        # Warning: The following filter makes the empty string (empty list)
+        # match *all* networks. This preserves backward compatibility with
+        # the previous (non-list) API but is counterintuitive for a list-
+        # oriented API.
+        lambda x: len(x) == 0 or cng.network_name.in_(x.split(','))
+    ),
+
+    'input-var': FormFilter(
+        # Single variable identifier
+        'input-var',
+        r'[a-z: _]+',
+        lambda x: cng.vars.like('%{}%'.format(x))
+    ),
+
+    'input-vars': FormFilter(
+        # Comma-separated list (as string) of variable identifiers
+        'input-vars',
+        # The following simple regex for a comma-separated list does not exclude
+        # empty list items (two successive commas) ... but it is simple.
+        r'[a-z: _,]*',
+        # The following filter checks whether any variable name `v` in the
+        # comma-separated list `x` of variable names occurs in `cng.vars`
+        # (which is itself a comma-separated list of variable names).
+        #
+        # Warning: This filter makes the empty string (empty list)
+        # match *all* variables. This makes it consistent with
+        # the previous (non-list) API but is counterintuitive for a list-
+        # oriented API.
+        lambda x: len(x) == 0 or or_(
+            *map(lambda v: cng.vars.like('%{}%'.format(v)), x.split(','))
+        )
+    ),
+
+    'input-freq': FormFilter(
+        # Comma-separated list (as string) of valid frequency identifiers
+        'input-freq',
+        # The following simple regex for a comma-separated list does not exclude
+        # empty list items (two successive commas) ... but it is simple.
+        r'(1-hourly|irregular|daily|12-hourly|,)*',
+        # Warning: The following filter makes the empty string (empty list)
+        # match *all* frequencies. This preserves backward compatibility with
+        # the previous (non-list) API but is counterintuitive for a list-
+        # oriented API.
+        lambda x: len(x) == 0 or cng.freq.in_(x.split(','))
+    ),
+
+    'input-polygon': FormFilter(
+        'input-polygon',
+        mk_mp_regex(),
+        "ST_intersects(ST_GeomFromText('%s', 4326), the_geom)"
+    ),
+
+    'only-with-climatology': FormFilter(
+        'only-with-climatology',
+        'only-with-climatology',
+        lambda x: or_(cng.vars.like('%within%'), cng.vars.like('%over%'))
+    ),
+}
 
 def validate_vars(environ):
     '''Iterate over the POST variables and convert them to SQL constraints
--- tests/test_filters.py
+++ tests/test_filters.py
@@ -14,20 +14,46 @@ def test_can_instantiate_filters():
     #assert isinstance(form_filters['input-polygon'](''), sqlalchemy.sql.expression.BinaryExpression)
 
 @pytest.mark.parametrize(('input', 'expected'), [
-    # No valid filters
+    # No filters
     ({}, []),
-    # One valid filter
-    ({'network-name': 'EC_raw'}, [ cng.network_name == 'EC_raw' ]),
+
+    # Valid values for each filter, singly
+    ({'from-date': '2000/01/01'}, [ cng.max_obs_time > datetime(2000, 1, 1) ]),
+
+    ({'to-date': '2000/01/01'}, [ cng.min_obs_time < datetime(2000, 1, 1) ]),
+
+    ({'network-name': ''}, [ True ]),
+    ({'network-name': 'EC_raw'}, [ cng.network_name.in_(['EC_raw']) ]),
+    ({'network-name': 'EC_raw,ARDA'}, [ cng.network_name.in_(['EC_raw', 'ARDA']) ]),
+
+    ({'input-var': 'variable'}, [ cng.vars.like('%variable%') ]), # OK
+
+    ({'input-vars': ''}, [ True ]),
+    ({'input-vars': 'var1'}, [ cng.vars.like('%var1%') ]),
+    ({'input-vars': 'var1,var2'}, [ or_(cng.vars.like('%var1%'), cng.vars.like('%var2%')) ]), # OK
+
+    ({'input-freq': ''}, [ True ]),
+    ({'input-freq': '1-hourly'}, [ cng.freq.in_(['1-hourly']) ]),
+    ({'input-freq': '1-hourly,irregular'}, [ cng.freq.in_(['1-hourly', 'irregular']) ]),
+
+    ({'only-with-climatology': 'only-with-climatology'}, [ or_(cng.vars.like('%within%'), cng.vars.like('%over%')) ]),
+
+    # Invalid values for each filter, singly
+    ({'from-date': '2000/Jan/01'}, []),
+    ({'to-date': '2000/Jan/01'}, []),
+    ({'network-name': '%argle&'}, []),
+    ({'input-var': '%&Invalid variable! name!'}, []),
+    ({'input-vars': 'blerg!,&frabble'}, []),
+    ({'input-freq': 'infrequent'}, []),
+    ({'only-with-climatology': 'invalid value'}, []),
+
     # One valid and one invalid filter
     ({'to-date': '2000/01/01', 'non-existant-filter': 'bar'}, [  cng.min_obs_time < datetime(2000, 1, 1) ]),
+
     # Several valid filters
     ({'input-freq': 'irregular', 'only-with-climatology': 'only-with-climatology', 'from-date': '1890/12/25'},
-     [cng.freq == 'irregular', or_(cng.vars.like('%within%'), cng.vars.like('%over%')), cng.max_obs_time > datetime(1890, 12, 25)]),
-    #({'input-var': ''}, []),
-    # valid filter names with invalid data
-    ({'input-var': '%&Invalid variable! name!'}, []),
-    ({'only-with-climatology': 'invalid value'}, [])
-    ])
+     [cng.freq.in_(['irregular']), or_(cng.vars.like('%within%'), cng.vars.like('%over%')), cng.max_obs_time > datetime(1890, 12, 25)]),
+])
 def test_validate_vars(input, expected):
     req = Request.blank('?' + urlencode(input))
     result = validate_vars(req.environ)
