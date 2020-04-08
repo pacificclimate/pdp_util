68bae88e0157b6f7474456abff02b88b90307bde
Rod Glover <rglover@uvic.ca>
2020-04-08 14:34:33 -0700

    Test raster.ensemble_files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 from pkg_resources import resource_filename
 
@@ -5,6 +6,13 @@ import pytest
 import pycds
 from pycds import Network, Contact, Station, History, Variable
 import modelmeta
+from modelmeta import (
+    Ensemble,
+    DataFile,
+    DataFileVariableDSGTimeSeries,
+    VariableAlias,
+    EnsembleDataFileVariables,
+)
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -60,6 +68,207 @@ def test_session(empty_session):
 @pytest.fixture(scope="function")
 def conn_params(test_session):
     yield test_session.get_bind().url
+
+#######################################################################
+# Test fixtures for code dependent on modelmeta database
+
+# TODO: Factor out common engine creation
+@pytest.fixture(scope='session')
+def mm_engine():
+    """Test-session-wide database engine"""
+    with testing.postgresql.Postgresql() as pg:
+        engine = create_engine(pg.url())
+        engine.execute("create extension postgis")
+        engine.execute(CreateSchema('test_meta'))
+        modelmeta.Base.metadata.create_all(bind=engine)
+        yield engine
+        engine.dispose()
+
+
+@pytest.fixture(scope='session')
+def mm_empty_session(mm_engine):
+    """Single-test database session. All session actions are rolled back on teardown"""
+    session = sessionmaker(bind=mm_engine)()
+    # Default search path is `"$user", public`. Need to reset that to search crmp (for our db/orm content) and
+    # public (for postgis functions)
+    session.execute('SET search_path TO test_meta, public')
+    # print('\nsearch_path', [r for r in session.execute('SHOW search_path')])
+    yield session
+    session.rollback()
+    session.close()
+
+
+# Ensemble
+
+def make_ensemble(id):
+    return Ensemble(
+        id=id,
+        changes='wonder what this is for',
+        description='Ensemble {}'.format(id),
+        name='ensemble{}'.format(id),
+        version=float(id)
+    )
+
+
+@pytest.fixture(scope='function')
+def ensemble1():
+    return make_ensemble(1)
+
+
+@pytest.fixture(scope='function')
+def ensemble2():
+    return make_ensemble(2)
+
+
+# DataFile
+
+def make_data_file(i, run=None, timeset=None):
+    return DataFile(
+        id=i,
+        filename='data_file_{}'.format(i),
+        first_1mib_md5sum='first_1mib_md5sum',
+        unique_id='unique_id_{}'.format(i),
+        x_dim_name='lon',
+        y_dim_name='lat',
+        t_dim_name='time',
+        index_time=datetime.datetime.now(),
+        run=run,
+        timeset=timeset,
+    )
+
+
+@pytest.fixture(scope='function')
+def data_file_1():
+    return make_data_file(1)
+
+
+@pytest.fixture(scope='function')
+def data_file_2():
+    return make_data_file(2)
+
+
+@pytest.fixture(scope='function')
+def data_file_3():
+    return make_data_file(3)
+
+
+# VariableAlias
+
+def make_variable_alias(i):
+    return VariableAlias(
+        long_name='long_name_{}'.format(i),
+        standard_name='standard_name_{}'.format(i),
+        units='units_{}'.format(i),
+    )
+
+
+@pytest.fixture(scope='function')
+def variable_alias_1():
+    return make_variable_alias(1)
+
+
+@pytest.fixture(scope='function')
+def variable_alias_2():
+    return make_variable_alias(2)
+
+
+# DataFileVariableDSGTimeSeries
+
+def make_test_dfv_dsg_time_series(i, file=None, variable_alias=None):
+    return DataFileVariableDSGTimeSeries(
+        id=i,
+        derivation_method='derivation_method_{}'.format(i),
+        variable_cell_methods='variable_cell_methods_{}'.format(i),
+        netcdf_variable_name='var_{}'.format(i),
+        disabled=False,
+        range_min=0,
+        range_max=100,
+        file=file,
+        variable_alias=variable_alias,
+    )
+
+
+@pytest.fixture(scope='function')
+def dfv_dsg_time_series_11(data_file_1, variable_alias_1):
+    return make_test_dfv_dsg_time_series(
+        1, file=data_file_1, variable_alias=variable_alias_1)
+
+
+@pytest.fixture(scope='function')
+def dfv_dsg_time_series_12(data_file_1, variable_alias_2):
+    return make_test_dfv_dsg_time_series(
+        2, file=data_file_1, variable_alias=variable_alias_2)
+
+
+@pytest.fixture(scope='function')
+def dfv_dsg_time_series_21(data_file_2, variable_alias_1):
+    return make_test_dfv_dsg_time_series(
+        3, file=data_file_2, variable_alias=variable_alias_1)
+
+
+@pytest.fixture(scope='function')
+def dfv_dsg_time_series_31(data_file_3, variable_alias_1):
+    return make_test_dfv_dsg_time_series(
+        4, file=data_file_3, variable_alias=variable_alias_1)
+
+
+# EnsembleDataFileVariables: tag DFVs with Ensembles
+
+def make_ensemble_dfvs(ensemble, dfv):
+    return EnsembleDataFileVariables(
+        ensemble_id=ensemble.id,
+        data_file_variable_id=dfv.id,
+    )
+
+
+@pytest.fixture(scope='function')
+def ensemble_dfvs_1(ensemble1, dfv_dsg_time_series_11, dfv_dsg_time_series_21):
+    return [
+        make_ensemble_dfvs(ensemble1, dfv_dsg_time_series_11),
+        make_ensemble_dfvs(ensemble1, dfv_dsg_time_series_21),
+    ]
+
+
+# TODO: Make this, or something like it, drive ensemble_dfvs_1
+@pytest.fixture(scope='function')
+def ensemble1_data_files(data_file_1, data_file_2):
+    return {data_file_1, data_file_2}
+
+
+@pytest.fixture(scope='function')
+def ensemble_dfvs_2(ensemble2, dfv_dsg_time_series_21, dfv_dsg_time_series_31):
+    return [
+        make_ensemble_dfvs(ensemble2, dfv_dsg_time_series_21),
+        make_ensemble_dfvs(ensemble2, dfv_dsg_time_series_31),
+    ]
+
+
+# TODO: Make this, or something like it, drive ensemble_dfvs_2
+@pytest.fixture(scope='function')
+def ensemble2_data_files(data_file_3, data_file_2):
+    return {data_file_2, data_file_3}
+
+
+# Database sessions
+
+@pytest.fixture(scope="function")
+def mm_test_session(
+    mm_empty_session,
+    ensemble1,
+    ensemble2,
+    dfv_dsg_time_series_11,
+    dfv_dsg_time_series_12,
+    ensemble_dfvs_1,
+    ensemble_dfvs_2,
+):
+    mm_empty_session.add_all([ensemble1, ensemble2])
+    mm_empty_session.flush()
+    mm_empty_session.add_all([dfv_dsg_time_series_11, dfv_dsg_time_series_12])
+    mm_empty_session.flush()
+    mm_empty_session.add_all(ensemble_dfvs_1)
+    mm_empty_session.add_all(ensemble_dfvs_2)
+    mm_empty_session.flush()
+    yield mm_empty_session
 
 
 @pytest.fixture(scope="function")
--- tests/test_raster.py
+++ tests/test_raster.py
@@ -8,11 +8,15 @@ from webob.request import Request
 def config():
     return {'ensemble': 'bc_prism', 'root_url': 'http://basalt.pcic.uvic.ca:8080/data/'}
 
-def test_ensemble_files(mm_session):
-    result = ensemble_files(mm_session, 'bc_prism')
-    assert result == {'tmax_monClim_PRISM_historical_run1_197101-200012': '/home/data/climate/PRISM/dataportal/tmax_monClim_PRISM_historical_run1_197101-200012.nc',
-                      'pr_monClim_PRISM_historical_run1_197101-200012': '/home/data/climate/PRISM/dataportal/pr_monClim_PRISM_historical_run1_197101-200012.nc',
-                      'tmin_monClim_PRISM_historical_run1_197101-200012': '/home/data/climate/PRISM/dataportal/tmin_monClim_PRISM_historical_run1_197101-200012.nc'}
+
+def test_ensemble1_files(mm_test_session, ensemble1, ensemble1_data_files):
+    result = ensemble_files(mm_test_session, ensemble1.name)
+    assert result == {df.unique_id: df.filename for df in ensemble1_data_files}
+
+
+def test_ensemble2_files(mm_test_session, ensemble2, ensemble2_data_files):
+    result = ensemble_files(mm_test_session, ensemble2.name)
+    assert result == {df.unique_id: df.filename for df in ensemble2_data_files}
 
 
 def test_db_raster_catalog(mm_session, config):
@@ -20,7 +24,8 @@ def test_db_raster_catalog(mm_session, config):
     assert result == {'tmax_monClim_PRISM_historical_run1_197101-200012': 'http://basalt.pcic.uvic.ca:8080/data/tmax_monClim_PRISM_historical_run1_197101-200012.nc',
                       'pr_monClim_PRISM_historical_run1_197101-200012': 'http://basalt.pcic.uvic.ca:8080/data/pr_monClim_PRISM_historical_run1_197101-200012.nc',
                       'tmin_monClim_PRISM_historical_run1_197101-200012': 'http://basalt.pcic.uvic.ca:8080/data/tmin_monClim_PRISM_historical_run1_197101-200012.nc'}
-    
+
+
 def test_db_raster_configurator(mm_session):
     args = [mm_session, 'A name', 'Version 0.0.0.1', '0.0', 'bc_prism', 'http://basalt.pcic.uvic.ca:8080/data/']
     result = db_raster_configurator(*args)
@@ -29,6 +34,7 @@ def test_db_raster_configurator(mm_session):
                                              'version': 'Version 0.0.0.1',
                                              'ensemble': 'bc_prism',
                                              'api_version': '0.0'}.items()])
+
 
 def test_db_raster_configurator_handlers(mm_session):
     # Handlers must be tested seperately because they are a list and order cannot be guaranteed on database query
@@ -42,6 +48,7 @@ def test_db_raster_configurator_handlers(mm_session):
     result = db_raster_configurator(*args)
     assert all([x in result['handlers'] for x in handlers])
 
+
 @pytest.mark.parametrize('url', ['', '/url/is/irrellevant'])
 def test_ensemble_catalog(mm_dsn, config, url):
     app = EnsembleCatalog(mm_dsn, config)
@@ -54,6 +61,7 @@ def test_ensemble_catalog(mm_dsn, config, url):
                     'pr_monClim_PRISM_historical_run1_197101-200012': 'http://basalt.pcic.uvic.ca:8080/data/pr_monClim_PRISM_historical_run1_197101-200012.nc',
                     'tmin_monClim_PRISM_historical_run1_197101-200012': 'http://basalt.pcic.uvic.ca:8080/data/tmin_monClim_PRISM_historical_run1_197101-200012.nc'}
 
+
 def test_raster_metadata_minmax(raster_metadata):
     req = Request.blank('?request=GetMinMax&id=pr-tasmax-tasmin_day_BCSD-ANUSPLIN300-CanESM2_historical-rcp26_r1i1p1_19500101-21001231&var=tasmax')
     resp = req.get_response(raster_metadata)
@@ -64,6 +72,7 @@ def test_raster_metadata_minmax(raster_metadata):
     stats = json.loads(resp.body)
     assert len(stats) == 2
     assert set(stats.keys()) == set(['max', 'min'])
+
 
 def test_raster_metadata_minmax_w_units(raster_metadata):
     req = Request.blank('?request=GetMinMaxWithUnits&id=pr-tasmax-tasmin_day_BCSD-ANUSPLIN300-CanESM2_historical-rcp26_r1i1p1_19500101-21001231&var=tasmax')
@@ -76,20 +85,24 @@ def test_raster_metadata_minmax_w_units(raster_metadata):
     assert len(stats) == 3
     assert set(stats.keys()) == set(['max', 'min', 'units'])
 
+
 def test_raster_metadata_minmax_no_id(raster_metadata):
     req = Request.blank('?request=INVALID_REQUEST_TYPE&id=pr-tasmax-tasmin_day_BCSD-ANUSPLIN300-CanESM2_historical-rcp26_r1i1p1_19500101-21001231&var=tasmax')
     resp = req.get_response(raster_metadata)
     assert resp.status == '400 Bad Request'
+
 
 def test_raster_metadata_minmax_no_id(raster_metadata):
     req = Request.blank('?request=GetMinMax&var=tasmax')
     resp = req.get_response(raster_metadata)
     assert resp.status == '400 Bad Request'
 
+
 def test_raster_metadata_minmax_no_var(raster_metadata):
     req = Request.blank('?request=GetMinMax&id=pr-tasmax-tasmin_day_BCSD-ANUSPLIN300-CanESM2_historical-rcp26_r1i1p1_19500101-21001231')
     resp = req.get_response(raster_metadata)
     assert resp.status == '400 Bad Request'
+
 
 def test_raster_metadata_minmax_bad_id(raster_metadata):
     req = Request.blank('?request=GetMinMax&id=NOT_A_VALID_ID&var=tasmax')
