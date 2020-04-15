c1773076308587ea894012a46bd0ca77056b8ec9
Rod Glover <rglover@uvic.ca>
2020-04-15 14:35:00 -0700

    Remove setup.py TODO

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
     url="http://www.pacificclimate.org/",
     author="James Hiebert",
     author_email="hiebert@uvic.ca",
-    # TODO: Should we adhere to policy: no version numbers in setup.py requirements?
     install_requires=['webob',
                       'openid2rp',
                       'genshi',
