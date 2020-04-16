392d4d6eaa9921a72758a514131dc9b87f8ba3dc
Rod Glover <rglover@uvic.ca>
2020-04-16 14:21:39 -0700

    Better fixtures, WIP

--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -1,10 +1,11 @@
 import pytest
 
 
-@pytest.mark.parametrize('ensemble, status, content_type, keys', [
-    (0, '200 OK', 'application/json', {'emission_0', 'emission_1'}),
-    (1, '200 OK', 'application/json', {'emission_0', 'emission_1'}),
-    (2, '404 Not Found', 'application/json', None),
+# TODO: enable None case
+@pytest.mark.parametrize('ensemble, status, content_type', [
+    (0, '200 OK', 'application/json'),
+    (1, '200 OK', 'application/json'),
+    (2, '404 Not Found', 'application/json'),
     # (None, '400 Bad Request', None, None),
 ], indirect=['ensemble'])
 @pytest.mark.usefixtures('mm_test_session_committed')
@@ -15,13 +16,15 @@ def test_ensemble_member_lister(
     ensemble,
     status,
     content_type,
-    keys,
 ):
     url = query_params(('ensemble_name', ensemble.name))
     resp, body = \
         test_wsgi_app(ensemble_member_lister, url, status, content_type)
-    if keys is not None:
-        assert set(body.keys()) == keys
+    if status[:3] == '200':
+        data_files = {dfv.file for dfv in ensemble.data_file_variables}
+        runs = {df.run for df in data_files}
+        emissions = {run.emission for run in runs}
+        assert set(body.keys()) == {e.short_name for e in emissions}
     if status[:3] == '404':
         assert body['code'] == 404
         assert body['message'] == 'Not Found'
