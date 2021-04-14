ed969883aab132d43d648f05ab1a279f030e5597
cairosanders <cairosanders@hotmail.com>
2021-04-14 09:37:22 -0700

    Use pydap-extras instead of various handler packages

--- pdp_util/agg.py
+++ pdp_util/agg.py
@@ -1,6 +1,6 @@
-'''
+"""
 This module provides aggregation utilities to translate a single HTTP request into multiple OPeNDAP requests, returning a single response
-'''
+"""
 
 from itertools import chain
 from zipfile import ZipFile, ZIP_DEFLATED
@@ -15,19 +15,21 @@ from sqlalchemy.orm import sessionmaker
 from pdp_util.util import get_stn_list
 from pdp_util.filters import validate_vars
 from pycds import Variable, Network
-#from pydap.handlers.pcic import RawPcicSqlHandler, ClimoPcicSqlHandler
-#from pydap.handlers.sql import Engines
+from pydap_extras.handlers.pcic import RawPcicSqlHandler, ClimoPcicSqlHandler
+from pydap_extras.handlers.sql import Engines
 from pydap.model import DatasetType, SequenceType, BaseType
 from pydap.handlers.lib import BaseHandler
 
 from pdp_util.util import get_extension, get_clip_dates
 
+
 def null_start_response(status, response_headers, exc_info=None):
     return None
 
+
 class PcdsZipApp(object):
-    '''WSGI application which accepts a set of PCDS filters in the request and responds with a generator which streams the OPeNDAP responses one by one
-    '''
+    """WSGI application which accepts a set of PCDS filters in the request and responds with a generator which streams the OPeNDAP responses one by one"""
+
     def __init__(self, dsn, sesh=None):
         self.dsn = dsn
         if sesh:
@@ -40,42 +42,49 @@ class PcdsZipApp(object):
         return Session()
 
     def __call__(self, environ, start_response):
-        '''Fire off pydap requests and return an iterable (from :func:`ziperator`)'''
+        """Fire off pydap requests and return an iterable (from :func:`ziperator`)"""
         req = Request(environ)
         form = req.params
-        climo = True if 'download-climatology' in form else False
+        climo = True if "download-climatology" in form else False
 
         filters = validate_vars(environ)
         stns = get_stn_list(self.session, filters)
 
         ext = get_extension(environ)
         if not ext:
-            return HTTPBadRequest("Requested extension not supported")(environ, start_response)
+            return HTTPBadRequest("Requested extension not supported")(
+                environ, start_response
+            )
 
-        status = '200 OK'
-        response_headers = [('Content-type','application/zip'), ('Content-Disposition', 'filename="pcds_data.zip"')]
+        status = "200 OK"
+        response_headers = [
+            ("Content-type", "application/zip"),
+            ("Content-Disposition", 'filename="pcds_data.zip"'),
+        ]
         start_response(status, response_headers)
-        environ['pydap.handlers.pcic.dsn'] = self.dsn
+        environ["pydap.handlers.pcic.dsn"] = self.dsn
 
-        responders = chain(get_all_metadata_index_responders(self.session, stns, climo),
-                           get_pcds_responders(self.dsn, stns, ext, get_clip_dates(environ), environ)
-                           )
+        responders = chain(
+            get_all_metadata_index_responders(self.session, stns, climo),
+            get_pcds_responders(self.dsn, stns, ext, get_clip_dates(environ), environ),
+        )
 
         return ziperator(responders)
 
-def ziperator(responders):
-    '''This method creates and returns an iterator which yields bytes for a :py:class:`ZipFile` that contains a set of files from OPeNDAP requests. The method will spool the first one gigabyte in memory using a :py:class:`SpooledTemporaryFile`, after which it will use disk.
 
-       :param responders: A list of (``name``, ``generator``) pairs where ``name`` is the filename to use in the zip archive and ``generator`` should yield all bytes for a single file.
-       :rtype: iterator
-    '''
-    with SpooledTemporaryFile(1024*1024*1024) as f:
-        yield b'PK' # Response headers aren't sent until the first chunk of data is sent.  Let's get this repsonse moving!
-        z = ZipFile(f, 'w', ZIP_DEFLATED)
+def ziperator(responders):
+    """This method creates and returns an iterator which yields bytes for a :py:class:`ZipFile` that contains a set of files from OPeNDAP requests. The method will spool the first one gigabyte in memory using a :py:class:`SpooledTemporaryFile`, after which it will use disk.
+
+    :param responders: A list of (``name``, ``generator``) pairs where ``name`` is the filename to use in the zip archive and ``generator`` should yield all bytes for a single file.
+    :rtype: iterator
+    """
+    with SpooledTemporaryFile(1024 * 1024 * 1024) as f:
+        yield b"PK"  # Response headers aren't sent until the first chunk of data is sent.  Let's get this repsonse moving!
+        z = ZipFile(f, "w", ZIP_DEFLATED)
 
         for name, responder in responders:
             pos = 2 if f.tell() == 0 else f.tell()
-            z.writestr(name, ''.join([x for x in responder]))
+            z.writestr(name, "".join([x for x in responder]))
             f.seek(pos)
             yield f.read()
         pos = f.tell()
@@ -83,54 +92,74 @@ def ziperator(responders):
         f.seek(pos)
         yield f.read()
 
+
 def get_all_metadata_index_responders(sesh, stations, climo=False):
-    '''This function is a generator which yields (``name``, ``generator``) pairs where ``name`` is the filename (e.g. [``network_name``].csv) and ``generator`` streams a csv file with information on the network's variables
+    """This function is a generator which yields (``name``, ``generator``) pairs where ``name`` is the filename (e.g. [``network_name``].csv) and ``generator`` streams a csv file with information on the network's variables
 
     :param stations: A list of (``network_name``, ``native_id``) pairs representing the stations for which this response should include variable metadata
     :param climo: Should these be climatological variables?
     :type climo: bool
     :rtype: iterator
-    '''
+    """
     for station in stations:
         network_name = station[0]
-        filename = '{0}/variables.csv'.format(network_name)
+        filename = "{0}/variables.csv".format(network_name)
         yield (filename, metadata_index_responder(sesh, network_name, climo))
 
+
 def metadata_index_responder(sesh, network, climo=False):
-    '''The function creates a pydap csv response which lists variable metadata out of the database. It returns an generator for the contents of the file
+    """The function creates a pydap csv response which lists variable metadata out of the database. It returns an generator for the contents of the file
 
     :param sesh: database session
     :type sesh: sqlalchemy.orm.session.Session
     :param network: Name of the network for which variables should be listed
     :type network: str
     :rtype: generator
-    '''
+    """
     maxlen = 256
     if climo:
-        climo_filt = or_(Variable.cell_method.like('%within%'), Variable.cell_method.like('%over%'))
-    else:
-        climo_filt = not_(or_(Variable.cell_method.like('%within%'), Variable.cell_method.like('%over%')))
-
-    rv = sesh.query(Variable).join(Network).filter(Network.name == network).filter(climo_filt)
-    a = np.array([(var.name, var.standard_name, var.cell_method, var.unit) for var in rv],
-                 dtype=np.dtype({'names': ['variable', 'standard_name', 'cell_method', 'unit'],
-                                 'formats':[(str, maxlen), (str, maxlen), (str, maxlen), (str, maxlen)]})
+        climo_filt = or_(
+            Variable.cell_method.like("%within%"), Variable.cell_method.like("%over%")
         )
-    dst = DatasetType('Variable metadata')
-    seq = SequenceType('variables')
-    seq['variable']      = BaseType('variable')
-    seq['standard_name'] = BaseType('standard_name', reference='http://llnl.gov/')
-    seq['cell_method']   = BaseType('cell_method', reference='http://llnl.gov/')
-    seq['unit']          = BaseType('unit')
+    else:
+        climo_filt = not_(
+            or_(
+                Variable.cell_method.like("%within%"),
+                Variable.cell_method.like("%over%"),
+            )
+        )
+
+    rv = (
+        sesh.query(Variable)
+        .join(Network)
+        .filter(Network.name == network)
+        .filter(climo_filt)
+    )
+    a = np.array(
+        [(var.name, var.standard_name, var.cell_method, var.unit) for var in rv],
+        dtype=np.dtype(
+            {
+                "names": ["variable", "standard_name", "cell_method", "unit"],
+                "formats": [(str, maxlen), (str, maxlen), (str, maxlen), (str, maxlen)],
+            }
+        ),
+    )
+    dst = DatasetType("Variable metadata")
+    seq = SequenceType("variables")
+    seq["variable"] = BaseType("variable")
+    seq["standard_name"] = BaseType("standard_name", reference="http://llnl.gov/")
+    seq["cell_method"] = BaseType("cell_method", reference="http://llnl.gov/")
+    seq["unit"] = BaseType("unit")
     seq.data = a
-    dst['variables'] = seq
+    dst["variables"] = seq
     responder = BaseHandler(dst)
- 
-    environ = {'PATH_INFO': '/variables.foo.ascii', 'REQUEST_METHOD': 'GET'}
+
+    environ = {"PATH_INFO": "/variables.foo.ascii", "REQUEST_METHOD": "GET"}
     return responder(environ, null_start_response).__iter__()
 
+
 def get_pcds_responders(dsn, stns, extension, clip_dates, environ):
-    '''Iterator object which coalesces a list of stations, compresses them, and returns the data for the response
+    """Iterator object which coalesces a list of stations, compresses them, and returns the data for the response
 
     :param dsn:
     :param stations: A list of (``network_name``, ``native_id``) pairs representing the stations for which this response should include variable metadata
@@ -140,30 +169,37 @@ def get_pcds_responders(dsn, stns, extension, clip_dates, environ):
     :param environ: WSGI environment variables which optionally set the ``download-climatology`` field
     :type environ: dict
     :rtype: iterator
-    '''
+    """
     req = Request(environ)
     form = req.params
 
-    handler, handler_ext = (ClimoPcicSqlHandler(dsn), 'csql') if 'download-climatology' in form else (RawPcicSqlHandler(dsn), 'rsql')
+    handler, handler_ext = (
+        (ClimoPcicSqlHandler(dsn), "csql")
+        if "download-climatology" in form
+        else (RawPcicSqlHandler(dsn), "rsql")
+    )
 
     sdate, edate = clip_dates
     content_length = 0
     for net, stn in stns:
         newenv = environ.copy()
-        newenv['PATH_INFO'] = '/%s/%s.%s.%s' % (net, stn, handler_ext, extension)
+        newenv["PATH_INFO"] = "/%s/%s.%s.%s" % (net, stn, handler_ext, extension)
 
         qs = []
-        if sdate: qs.append("station_observations.time>='%s'" % sdate)
-        if edate: qs.append("station_observations.time<='%s'" % edate)
-        newenv['QUERY_STRING'] = '&'.join(qs)
+        if sdate:
+            qs.append("station_observations.time>='%s'" % sdate)
+        if edate:
+            qs.append("station_observations.time<='%s'" % edate)
+        newenv["QUERY_STRING"] = "&".join(qs)
 
-        name = '%(net)s/%(stn)s.%(extension)s' % locals()
+        name = "%(net)s/%(stn)s.%(extension)s" % locals()
         yield (name, handler(newenv, null_start_response))
 
-def agg_generator(global_conf, **kwargs):
-    '''Factory function for the :class:`PcdsZipApp`
 
-       :param global_conf: dict containing the key conn_params which is passed on to :class:`PcdsZipApp`. Everything else is ignored.
-       :param kwargs: ignored
-    '''
-    return PcdsZipApp(dsn=global_conf['dsn'])
+def agg_generator(global_conf, **kwargs):
+    """Factory function for the :class:`PcdsZipApp`
+
+    :param global_conf: dict containing the key conn_params which is passed on to :class:`PcdsZipApp`. Everything else is ignored.
+    :param kwargs: ignored
+    """
+    return PcdsZipApp(dsn=global_conf["dsn"])
