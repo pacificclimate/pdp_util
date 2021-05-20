e2b1f80b3556a959643215938ea5b5c574072bc5
James Hiebert <james@hiebert.name>
2021-05-20 13:47:48 -0700

    Filter out unpublished networks

---
+++ tests/test_geo.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.schema import CreateSchema
+import testing.postgresql
+import pycds
+
+
+def test_that_I_can_use_PostGIS(empty_session):
+    history = pycds.History(
+        station_name="FIVE MILE", the_geom="SRID=4326;POINT(-122.68889 50.91089)"
+    )
+    empty_session.add(history)
+    empty_session.commit()
+    return
